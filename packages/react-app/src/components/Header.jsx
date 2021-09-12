7e5c161feacf80c814d29001258cddcc71cd4649
zhware <zh@zhware.net>
2021-09-12 20:53:12 +0900

    SPA wallet on SmartBCH example

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -5,8 +5,8 @@ import React from "react";
 
 export default function Header() {
   return (
-    <a href="https://github.com/zh/scaffold-eth/tree/multi-evm" target="_blank" rel="noopener noreferrer">
-      <PageHeader title="scaffold-multi" subTitle="forkable EVM dev stack" style={{ cursor: "pointer" }} />
+    <a href="https://github.com/zh/scaffold-eth/tree/multi-wallet" target="_blank" rel="noopener noreferrer">
+      <PageHeader title="SPA Wallet" style={{ cursor: "pointer" }} />
     </a>
   );
 }
