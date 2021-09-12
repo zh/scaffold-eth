ad878755c1c928bc1738e80495e083c68ec203b0
zhware <zh@zhware.net>
2021-09-12 19:22:08 +0900

    Example for SEP-20 tokens wallet

--- packages/hardhat/deploy/00_deploy_scaffold_token.js
+++ packages/hardhat/deploy/00_deploy_scaffold_token.js
@@ -3,9 +3,9 @@
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  await deploy("YourNFTs", {
+  await deploy("ScaffoldToken", {
     from: deployer,
     log: true,
   });
 };
-module.exports.tags = ["YNFT"];
+module.exports.tags = ["SCF"];
