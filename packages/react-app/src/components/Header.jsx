b4559e8f1c9559c757ac7928b4986c2c253cbfb1
zhware <zh@zhware.net>
2021-09-12 19:27:25 +0900

    Header component fixed

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
   return (
     <a href="https://github.com/zh/scaffold-eth/tree/ava" target="_blank" rel="noopener noreferrer">
       <PageHeader
-        title="scaffold-ava"
-        subTitle="forkable Avalanche dev stack"
+        title="AVA-20"
+        subTitle="ERC-20 token on Avalanche"
         style={{ cursor: "pointer" }}
         avatar={{
           src: "https://wallet.avax.network/img/avax_icon_circle.png",
