feeecb03f716c4e3017240cc244b77ea9cddfa5c
zhware <zh@zhware.net>
2021-09-30 21:02:57 +0900

    Header text fixed

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -5,8 +5,8 @@ import React from "react";
 
 export default function Header() {
   return (
-    <a href="https://github.com/zh/scaffold-eth/tree/multi-evm" target="_blank" rel="noopener noreferrer">
-      <PageHeader title="scaffold-multi" subTitle="forkable EVM dev stack" style={{ cursor: "pointer" }} />
+    <a href="https://github.com/zh/scaffold-eth/tree/multi-20" target="_blank" rel="noopener noreferrer">
+      <PageHeader title="scaffold-multi" subTitle="ERC-20 example" style={{ cursor: "pointer" }} />
     </a>
   );
 }
